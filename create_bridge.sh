33e7e8e4fc3949e306540d929ff3539ec584140c
黄 扬 <39240132+JeromeFly@users.noreply.github.com>
2018-11-16 10:03:22 +0800

    Update create_bridge.sh

--- create_bridge.sh
+++ create_bridge.sh
@@ -4,7 +4,7 @@
 
 yum install bridge-utils net-tools -y
 yum remove firewalld-filesystem NetworkManager-libnm -y   #若为GUI的发行版，卸载NetworkManager-libnm会导致GUI损坏
-sed 's/SELINUX=enforcing/SELINUX=disabled/' /etc/selinux/config
+sed -i 's/SELINUX=enforcing/SELINUX=disabled/' /etc/selinux/config
 setenforce 0
 
 for i in `seq 0`;do
